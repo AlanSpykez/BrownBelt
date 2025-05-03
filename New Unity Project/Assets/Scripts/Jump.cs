7cc593eb5de7694bc0f806fdc90a81f3c3e7e991
MasterHacker10 <164410221+Hugokh36@users.noreply.github.com>
2025-05-03 11:49:22 -0400

    yada yada

---
+++ New Unity Project/Assets/Scripts/Jump.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float jumpForce = 5.7f;
+
+    public bool isGrounded;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 0.1f, transform.position.z), Vector3.down, .55f);
+        Debug.DrawRay(new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z), Vector3.down * .55f, Color.red);
+
+        Debug.Log(Physics.RaycastAll(new Vector3(transform.position.x, transform.position.y + 0.1f, transform.position.z), Vector3.down, .55f));
+
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            Debug.Log("work please");
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
