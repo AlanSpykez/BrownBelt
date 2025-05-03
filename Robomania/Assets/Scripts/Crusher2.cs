1d88f5df56b7d3de3a1bd94b1479517ab25e4544
DaKillerDonut <dylantaocallahan@gmail.com>
2025-05-03 10:17:25 -0400

    hhh

---
+++ Robomania/Assets/Scripts/Crusher2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crusher2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10f;
+    void Start() {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.right * speed, ForceMode2D.Impulse); 
+    }
+}
