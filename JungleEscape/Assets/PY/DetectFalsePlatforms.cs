729bc64ea75aef68ec41ee45dcdb0c6eae0110ba
MasterHacker10 <alanjunhee@icloud.com>
2025-05-17 13:55:21 -0400

    s

---
+++ JungleEscape/Assets/PY/DetectFalsePlatforms.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectFalsePlatforms : MonoBehaviour
+{
+    public bool hit;
+    public LayerMask ahhh;
+    void Update()
+    {
+        hit = Physics.Raycast(transform.position,transform.forward, 2f);
+        Debug.DrawRay(transform.position, transform.forward * 2f, Color.green);
+        if (hit) { 
+            Debug.Log("for fahkey");
+
+        } else {
+            Debug.Log("for reeaal");
+        }
+    }
+}
