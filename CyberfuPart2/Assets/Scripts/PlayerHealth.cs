d42f53054aaa31e2d23260cde50e25f8086dae23
DaKillerDonut <alanjunhee@icloud.com>
2025-05-24 14:11:04 -0400

    ah
    ahh

---
+++ CyberfuPart2/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxPlayerHealth = 10;
+    public int currentPlayerHealth;
+   
+    void Start()
+    {
+        currentPlayerHealth = maxPlayerHealth;
+    }
+    
+}
